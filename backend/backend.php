2f8b404959735ce96e59faedb4424e70e4a69765
Patrick Conner <pconner03@gmail.com>
2014-02-24 20:27:10 -0500

    Added rest function to retrieve user key with proper username/password

--- backend/backend.php
+++ backend/backend.php
@@ -16,4 +16,21 @@ function getActivity($acName){
 	echo json_encode($result);
 }
 
+/**
+ * Retrieves a user's unique ID, which can be used for stateless data retrieval
+ * from the REST api
+ */
+function getUserKey($username, $password){
+	$dbQuery = sprintf("SELECT PersonID from Person WHERE 
+		LoginID='%s' AND PasswordHash=SHA2('%s', 256)", 
+		mysql_real_escape_string($username), 
+		mysql_real_escape_string($password));
+	//We are using SHA-256 hashing
+
+	$result = getDBResultsArray($dbQuery);
+	header("Content-type: text/plain");
+	echo json_encode($result[0]); //Since there should only be one result, just return first array element
+}
+
+
 ?>
