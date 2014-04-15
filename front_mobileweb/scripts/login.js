be45db928b784993c2c783dfe7a6431e4f50bde5
Melaena <melaena.roberts@gmail.com>
2014-04-15 02:14:46 -0400

    Unecessary url vars here.

--- front_mobileweb/scripts/login.js
+++ front_mobileweb/scripts/login.js
@@ -1,15 +1,3 @@
-function toPHPFormat(d){
-	var monthStr = "" +(d.getMonth()+1);
-	if (monthStr.length<2){
-		monthStr = "0" + monthStr;
-	}
-	var dateStr = "" + d.getDate();
-	if (dateStr.length<2){
-		dateStr = "0" + dateStr;
-	}
-	return d.getFullYear() + "-" + monthStr + "-" + dateStr;
-}
-
 $(document).ready(function () {
 
 	$("#loginForm").submit(function(e){
@@ -22,10 +10,8 @@ $(document).ready(function () {
 		
 		$.getJSON( loginAPI, str).done(function( data ) {
 			if (data.sessionID!=undefined){
-				//get date
-				var d = new Date();
 				//add url params
-				var url = "index.html?date=" +toPHPFormat(d);
+				var url = "index.html";
 				//redirect
 				window.location.replace(url);
 			}
